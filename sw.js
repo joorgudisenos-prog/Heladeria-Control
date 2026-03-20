7053efed2f0fedf65ee0e9b427ed96a9ec1b506a
joorgudisenos-prog <joorgudisenos@gmail.com>
2026-03-20 11:51:11 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
+const CACHE_NAME = 'helados-cali-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/heladeria.css'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Borrando cache antiguo:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
