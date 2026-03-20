4fe24d5058b9b84be9c50bdebc6b62167960fa51
joorgudisenos-prog <joorgudisenos@gmail.com>
2026-03-20 12:08:31 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,44 @@
 const CACHE_NAME = 'helados-cali-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/heladeria.css'
+    '/',
+    '/index.html'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache abierto');
-        return cache.addAll(urlsToCache);
-      })
-  );
+    console.log('Service Worker instalado');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Borrando cache antiguo:', cache);
-            return caches.delete(cache);
-          }
+    console.log('Service Worker activado');
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Borrando cache antiguo:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
